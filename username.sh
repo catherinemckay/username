9288bacdd9d627ffa406bd6ace0e36fdb47923a4
catherinemckay <camckay@chapman.edu>
2020-11-19 21:43:10 -0800

    First commit

--- username.sh
+++ username.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
 # username.sh
 # catherinemckay
+echo "Your username most only contain lowercase letters, digits, and underscores."
+echo "Your username must contain no more than 12 characters."
+echo "Your username must start with a lowercase letter."
 echo "Enter a Username: "
-read NAME
-while echo "$NAME" | egrep -E -v "^[a-z][0-9]{5}$"> /dev/null 2>&1
+read -r NAME 
+while echo "$NAME" | grep -E -v "^[a-z][0-9a-z_]{2,11}$"> /dev/null 2>&1
 do
-	echo "You must enter a valid ZIP code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
-	read NAME
+	echo "Your username most only contain lowercase letters, digits, and underscores."
+	echo "Your username must contain no more than 12 characters."
+	echo "Your username must start with a lowercase letter."
+	echo "Enter a Username: "
+	read -r NAME
 done
 echo "Thank you"
